e925803851c18dfd1d1f7931855b69c8889b3b11
marialombardi861 <marialombardi828@gmail.com>
2024-06-27 19:06:02 +0200

    update load data

--- Tirocinio/load_data.py
+++ Tirocinio/load_data.py
@@ -1,16 +1,34 @@
-import os
 import pymongo
 import json
+import os
+import glob
 
-# Connessione al database MongoDB
-client = pymongo.MongoClient("mongodb+srv://gerardofrino588:95VOEFttqh89tAsS@cluster0.fdg5slg.mongodb.net/DBMeteoAnalytics?retryWrites=true&w=majority")
-db = client["MeteoAnalytics"]
-collection = db["Sensori"]
+# Replace with your actual MongoDB connection details
+connection_string = "mongodb+srv://pippo:HSBFjpCRp7sIH2p5@cluster0.hqsn7ul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-# Caricamento del file JSON
-with open("Tirocinio\sensor_35.899246312094625_14.5141040186575.json", "r") as f:
-    data = json.load(f)
+try:
+    # Connect to MongoDB
+    client = pymongo.MongoClient(connection_string)
+    db = client.MeteoAnalytics  # Replace with your database name
+    collection = db.Inquinamento  # Replace with your collection name
 
-print(data)
+    # Directory containing the JSON files
+    json_directory = "C:\\Users\\USER\\Desktop\\sensori_puliti-20240627T164829Z-001\\sensori_puliti"
 
-#collection.insert_one(data)
+    # Iterate through all JSON files in the directory
+    for filepath in glob.glob(os.path.join(json_directory, "*.json")):
+        with open(filepath, "r") as f:
+            data = json.load(f)
+
+    #Check if the data is a list of documents or a single document
+            if isinstance(data, list):
+                collection.insert_many(data)
+                print(f"Documents from {os.path.basename(filepath)} inserted successfully!")
+            else:
+                collection.insert_one(data)
+                print(f"Document from {os.path.basename(filepath)} inserted successfully!")
+
+except pymongo.errors.OperationFailure as e:
+    print(f"Operation failure: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
